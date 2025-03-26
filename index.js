41edd58e380e4096112143ffdaa1844211632475
iannxz <iannarthur90@gmail.com>
2025-03-26 08:07:32 -0300

    Enviando para o repositorio remoto

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let num2;
 
 console.log(`A soma de ${num1} e ${num2} é ${num1 + num2}`)
 console.log(`A divisão de ${num1} / ${num2} = ${num1 / num2}`);
-console.log(`A multiplicação de ${num1} * ${num2} = ${num1 * num2}`);console.log(`A Subtração de ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`A multiplicação de ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`A Subtração de ${num1} - ${num2} = ${num1 - num2}`);
